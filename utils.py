Always sort agent types; fix bug in HT; use z=0.05 for softmax

utils.py
@@ -10,11 +10,11 @@ def clamp(val, minval, maxval):
 
 def normalize(array):
     """ Normalizes an array of values so that the sum of the array is unity. """
-    sum = float(sum(array))
-    return [x / sum for x in array]
+    total = float(sum(array))
+    return [x / total for x in array]
 
 
-def softmax_rv(masses, values=None, z=0.1):
+def softmax_rv(masses, values=None, z=0.05):
     """ Returns a discrete random variable based on the softmax function. """
     nums = np.exp(np.array(masses) / z)
     dist = nums / np.sum(nums)