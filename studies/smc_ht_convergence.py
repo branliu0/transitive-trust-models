8ae2abaaa68f19c34e920f545359e6544859dc4d
Brandon Liu <brandon.k.liu@gmail.com>
2013-12-08 13:46:34 -0500

    Compare monte carlo estimates to eigen HT

--- studies/smc_ht_convergence.py
+++ studies/smc_ht_convergence.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
 
+from hitting_time.eigen_hitting_time import personalized_eigen_ht
 from hitting_time.single_monte_carlo import complete_path_smc_hitting_time
 from hitting_time.single_monte_carlo import generative_smc_hitting_time
 from hitting_time.single_monte_carlo import naive_smc_hitting_time
@@ -22,6 +23,10 @@ def plot_convergence(num_iterations, trials=None, filename=None):
                for _ in xrange(num_iterations)]
               for _ in trials]
 
+    # Use eigen hitting time to compute a very accurate estimate
+    best_estimates = [[personalized_eigen_ht(g) for g in trial]
+                      for trial in graphs]
+
     methods = [
         # label name, function
         ("Naive", naive_smc_hitting_time),
@@ -32,20 +37,18 @@ def plot_convergence(num_iterations, trials=None, filename=None):
     # Run the simulations
     for name, func in methods:
         print name
-        raws = []
+        raws = np.zeros((len(trials), num_iterations, NUM_NODES, NUM_NODES))
         for i, t in enumerate(trials):
-            trial = []
             for j in xrange(num_iterations):
-                trial.append(func(graphs[i][j], t))
+                raws[i][j] = func(graphs[i][j], t)
                 sys.stdout.write('.'); sys.stdout.flush()
-            raws.append(trial)
         sys.stdout.write('\n')
 
         # Compute errors
         best = np.mean(raws[-1], axis=0)
         means = []
         errs = []
-        for trial in raws:
+        for trial, best in zip(raws, best_estimates):
             diffs = np.sum(np.absolute(trial - best), axis=(1, 2))
             diffs /= NUM_NODES * NUM_NODES  # avg error per node
             means.append(np.mean(diffs))
