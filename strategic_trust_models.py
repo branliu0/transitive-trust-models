51a36f0e4d7f9a720ac44be3a38f03383e4c15f3
Brandon Liu <brandon.k.liu@gmail.com>
2014-01-15 14:56:28 -0500

    Add thin edges to ensure connectedness

--- strategic_trust_models.py
+++ strategic_trust_models.py
@@ -6,8 +6,12 @@ perform optimal manipulations to make themselves look better, and then the
 trust model scores are applied.
 """
 import random
+import sys
 
-from hitting_time import personalized_LA_ht
+import networkx as nx
+import numpy as np
+
+from hitting_time.mat_hitting_time import personalized_LA_ht
 import trust_models as tm
 
 def cut_outlinks(graph, agents):
@@ -24,17 +28,30 @@ def generate_sybils(graph, agents, num_sybils):
         sybil_counter += num_sybils
 
 
+def add_thin_edges(graph, edge_weight=1e-5):
+    edges = graph.edges()
+    for i in graph.nodes_iter():
+        for j in graph.nodes_iter():
+            if i == j:
+                continue
+            if (i, j) not in edges:
+                graph.add_edge(i, j, weight=edge_weight)
+                # We don't bother with inv_weight because this method doesn't
+                # need to be used for shortest path.
+
+
 def global_pagerank(graph, num_strategic, sybil_pct):
     """ Global PageRank.
 
     Cut all outlinks + generate sybils.
     """
     graph = graph.copy()
+    N = graph.number_of_nodes()
     strategic_agents = random.sample(graph.nodes(), num_strategic)
     num_sybils = int(graph.number_of_nodes() * sybil_pct)
     cut_outlinks(graph, strategic_agents)
     generate_sybils(graph, strategic_agents, num_sybils)
-    return tm.pagerank(graph)
+    return tm.pagerank(graph)[:N]
 
 
 def person_pagerank(graph, num_strategic, sybil_pct):
@@ -43,10 +60,12 @@ def person_pagerank(graph, num_strategic, sybil_pct):
     Cut all outlinks + generate one sybil.
     """
     graph = graph.copy()
+    N = graph.number_of_nodes()
     strategic_agents = random.sample(graph.nodes(), num_strategic)
     cut_outlinks(graph, strategic_agents)
     generate_sybils(graph, strategic_agents, 1)
-    return tm.personalized_pagerank(graph)
+    add_thin_edges(graph)
+    return tm.personalized_pagerank(graph)[:N, :N]
 
 
 def global_hitting_time(graph, num_strategic, sybil_pct):
@@ -55,11 +74,12 @@ def global_hitting_time(graph, num_strategic, sybil_pct):
     Cut all outlinks + generate sybils.
     """
     graph = graph.copy()
+    N = graph.number_of_nodes()
     strategic_agents = random.sample(graph.nodes(), num_strategic)
     num_sybils = int(graph.number_of_nodes() * sybil_pct)
     cut_outlinks(graph, strategic_agents)
     generate_sybils(graph, strategic_agents, num_sybils)
-    return tm.hitting_pagerank(graph, 'all')
+    return tm.hitting_pagerank(graph, 'all')[:N]
 
 
 def person_hitting_time(graph, num_strategic, sybil_pct):
@@ -70,6 +90,7 @@ def person_hitting_time(graph, num_strategic, sybil_pct):
     graph = graph.copy()
     strategic_agents = random.sample(graph.nodes(), num_strategic)
     cut_outlinks(graph, strategic_agents)
+    add_thin_edges(graph)
     return personalized_LA_ht(graph)
 
 
@@ -79,9 +100,37 @@ def person_max_flow(graph, num_strategic, sybil_pct):
     Cut all outlinks.
     """
     graph = graph.copy()
+    N = graph.number_of_nodes()
     strategic_agents = random.sample(graph.nodes(), num_strategic)
+    saved_edges = {a: graph.edges(a, data=True) for a in strategic_agents}
     cut_outlinks(graph, strategic_agents)
-    return tm.max_flow(graph)
+    add_thin_edges(graph)
+
+    # Need to reimplement max flow here because we only want to cut outedges
+    # When we're not being evaluated.
+    scores = np.zeros((N, N))
+    for i in xrange(N):
+        # Add back in the edges for this agent, so we can get an actual score.
+        if i in strategic_agents:
+            for a, b, d in saved_edges[i]:
+                graph[a][b]['weight'] = d['weight']
+
+        # Now compute the max flow scores
+        for j in xrange(N):
+            if i == j:
+                scores[i][j] = None
+            else:
+                mf = nx.max_flow(graph, i, j, capacity='weight')
+                scores[i][j] = None if mf == 0 else mf
+
+        # Now remove those edges again (a bit inefficiently)
+        cut_outlinks(graph, [i])
+        add_thin_edges(graph)
+
+        sys.stdout.write('.')
+    sys.stdout.write("\n")
+
+    return scores
 
 
 def person_shortest_path(graph, num_strategic, sybil_pct):
