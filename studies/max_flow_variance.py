e3fd8462f9df16b1c10376c8154013b59aa843c4
Brandon Liu <brandon.k.liu@gmail.com>
2014-02-13 21:54:12 -0500

    Playing around with the MF variance study

--- studies/max_flow_variance.py
+++ studies/max_flow_variance.py
@@ -7,6 +7,7 @@ import numpy as np
 from scipy import stats
 
 from experiment_sets import EdgeCountExperimentSet
+import studies.efficiency as e
 from trust_graph import TrustGraph
 import trust_models as tm
 
@@ -19,34 +20,35 @@ def max_flow_variance(num_iters=5, edge_counts=None):
 
     raw_scores = np.zeros((len(edge_counts), num_iters, NUM_NODES, NUM_NODES))
     means = np.zeros((len(edge_counts), num_iters))
-    variances = np.zeros((len(edge_counts), num_iters))
+    # variances = np.zeros((len(edge_counts), num_iters))
     for i, count in enumerate(edge_counts):
         for j in xrange(num_iters):
             g = TrustGraph(NUM_NODES, 'uniform', 'uniform', count, 'noisy', 30)
             at = g.agent_types
             scores = tm.max_flow(g)
             raw_scores[i, j] = scores
-            corrs = [stats.spearmanr(at, score)[0] for score in scores]
-            means[i, j] = np.mean(corrs)
+            # corrs = [stats.spearmanr(at, score)[0] for score in scores]
+            # means[i, j] = np.mean(corrs)
+            means[i, j] = e.compute_informativeness(at, scores, False)
             # variances[i, j] = np.var(corrs)
-            variances[i, j] = np.mean(
-                [np.var([x for x in score if x is not None])
-                 for score in scores])
+            # variances[i, j] = np.mean(
+                # [np.var([x for x in score if x is not None])
+                 # for score in scores])
 
-            for s in scores:
-                plt.plot(count, np.var([x for x in score if x is not None]),
-                         'o', alpha=0.2)
+            # for s in scores:
+                # plt.plot(count, np.var([x for x in score if x is not None]),
+                         # 'o', alpha=0.2)
 
-    plt.suptitle('Variances of personalized max flow scores '
-                 'against graph density (n = %d)' % num_iters)
-    plt.ylabel('Variance of max flow scores')
-    plt.xlabel('Number of edges per node')
-    plt.xticks(edge_counts, edge_counts)
-    plt.margins(0.07)
-    plt.show()
+    # plt.suptitle('Variances of personalized max flow scores '
+                 # 'against graph density (n = %d)' % num_iters)
+    # plt.ylabel('Variance of max flow scores')
+    # plt.xlabel('Number of edges per node')
+    # plt.xticks(edge_counts, edge_counts)
+    # plt.margins(0.07)
+    # plt.show()
 
     means = means.mean(axis=1)
-    variances = variances.mean(axis=1)
+    # variances = variances.mean(axis=1)
 
     # Plot Means
     plt.plot(edge_counts, means, 'o--')
@@ -59,13 +61,13 @@ def max_flow_variance(num_iters=5, edge_counts=None):
     plt.show()
 
     # Plot Variances
-    plt.plot(edge_counts, variances, 'o--')
-    plt.suptitle('Variance of personalized max flow scores '
-                 'against graph density (n = %d)' % num_iters)
-    plt.ylabel('Average variance of personalized max flow scores')
-    plt.xlabel('Number of edges per node')
-    plt.xticks(edge_counts, edge_counts)
-    plt.margins(0.07)
-    plt.show()
+    # plt.plot(edge_counts, variances, 'o--')
+    # plt.suptitle('Variance of personalized max flow scores '
+                 # 'against graph density (n = %d)' % num_iters)
+    # plt.ylabel('Average variance of personalized max flow scores')
+    # plt.xlabel('Number of edges per node')
+    # plt.xticks(edge_counts, edge_counts)
+    # plt.margins(0.07)
+    # plt.show()
 
     return raw_scores
